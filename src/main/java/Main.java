6277e5561b7ea4ccbfce56530583365a627431d5
Sergey Tsoy <hekonnote@gmail.com>
2020-10-16 23:59:02 +0600

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Maven-проект!");
-        System.out.println("Maven-проект!");
-        System.out.println("Maven-проект!");
-        System.out.println("Maven-проект!");
-        System.out.println("Maven-проект!");
+
     }
 }
